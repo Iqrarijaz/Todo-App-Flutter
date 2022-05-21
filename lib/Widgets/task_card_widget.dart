2a3fa5e5b95c219df71a3cb021ac871a479230ca
Iqrarijaz <iqrarijaz788@gmail.com>
2022-05-21 17:19:21 +0500

    Commit

--- lib/Widgets/task_card_widget.dart
+++ lib/Widgets/task_card_widget.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+
 class TaskCardWidget extends StatelessWidget {
-  const TaskCardWidget({Key? key}) : super(key: key);
+  final String title;
+  final String description;
+
+  TaskCardWidget({required this.title, required this.description});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width:double.infinity,
-      padding:const EdgeInsets.symmetric(vertical:32,horizontal:24),
-      child:const Text("Get Started!",style:TextStyle(
-        fontSize:24.0,
-        fontWeight:FontWeight.bold,
-        color:Colors.black
-
-      )),
-      decoration: BoxDecoration(
-        color:Colors.white,
-        borderRadius: BorderRadius.circular(24.0)
-      ),
-    );
+        width: double.infinity,
+        margin: const EdgeInsets.only(bottom: 20.0),
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(24.0)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title.isEmpty ? "(Unnamed Task)" : title,
+                style: const TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+            const Padding(
+              padding: EdgeInsets.only(top: 10.0),
+            ),
+            Text(description.isEmpty ? "No Description Added": description,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  color: Color(0xFF868290),
+                  height: 1.5,
+                )),
+          ],
+        ));
   }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -19,17 +19,56 @@ class _HomePageState extends State<HomePage> {
                 const EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
             width: double.infinity,
             color: const Color(0xFFF6F6F6),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Stack(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 32.0,
-                  ),
-                  child:
-                      const Image(image: AssetImage('assets/images/logo.png')),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        bottom: 32.0,
+                      ),
+                      child: const Image(
+                          image: AssetImage('assets/images/logo.png')),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          TaskCardWidget(
+                              title: 'Get Started !',
+                              description:
+                              "Hello User ! Welcome to WHAT-TODO app, This is a default task that you can edit"),
+                          TaskCardWidget(
+                              title: 'Get Started !',
+                              description:
+                              "Hello User ! Welcome to WHAT-TODO app, This is a default task that you can edit"),
+                          TaskCardWidget(
+                              title: 'Get Started !',
+                              description:
+                              "Hello User ! Welcome to WHAT-TODO app, This is a default task that you can edit"),
+                          TaskCardWidget(
+                              title: 'Get Started !',
+                              description:
+                              "Hello User ! Welcome to WHAT-TODO app, This is a default task that you can edit"),
+                          TaskCardWidget(
+                            title: '',
+                            description: '',
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                const TaskCardWidget(),
+                Positioned(
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    width:60,
+                      height:60,
+                      decoration: BoxDecoration(color: const Color(0xFF7349FE),
+                        borderRadius: BorderRadius.circular(20),),
+                      child: const Image(image:AssetImage("assets/images/add_icon.png"))),
+                )
               ],
             )),
       ),
